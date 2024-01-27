b348ed62ad1536d2f4acddcd5e4299151f37b820
RaspingHare3640 <adithya.bollu@gmail.com>
2024-01-27 15:48:16 -0800

    Added a dropdown for the teams

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
-      <!-- <Header/>   -->
-      </div>
+    <form action="">
+      <label for="teams">Select a Team</label>
+      <select name="teams" id="dropdown" v-model="selectedTeam">
+        <option v-for="team in teams"  :value="team">{{ team }}</option>
+      </select>
+    </form>    
+    <p>{{ selectedTeam }}</p>
+  </div>
 </template>
 
 <script>
@@ -11,6 +17,44 @@ export default {
   name: 'HomeView',
   components: {
     Header
+  },
+  data () {
+    return {
+      selectedTeam: "",
+      teams : [
+      "Atlanta Hawks",
+      "Boston Celtics",
+      "Brooklyn Nets",
+      "Charlotte Hornets",
+      "Chicago Bulls",
+      "Cleveland Cavaliers",
+      "Dallas Mavericks",
+      "Denver Nuggets",
+      "Detroit Pistons",
+      "Golden State Warriors",
+      "Houston Rockets",
+      "Indiana Pacers",
+      "Los Angeles Clippers",
+      "Los Angeles Lakers",
+      "Memphis Grizzlies",
+      "Miami Heat",
+      "Milwaukee Bucks",
+      "Minnesota Timberwolves",
+      "New Orleans Pelicans",
+      "New York Knicks",
+      "Oklahoma City Thunder",
+      "Orlando Magic",
+      "Philadelphia 76ers",
+      "Phoenix Suns",
+      "Portland Trail Blazers",
+      "Sacramento Kings",
+      "San Antonio Spurs",
+      "Toronto Raptors",
+      "Utah Jazz", 
+      "Washington Wizards"],
+    }
+    
+    
   }
 }
 </script>
