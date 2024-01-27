62a4826b22a98996258d3bd9cd8064e2f5ff9f02
Akhil Nekkanti <70420358+Akhil-Nekkanti@users.noreply.github.com>
2024-01-27 15:47:01 -0800

    TeamCSV added
    Changed file names for readability, created a file with teamdata from the current season

--- main.py
+++ main.py
@@ -9,19 +9,32 @@ import pandas as pd
 data = pd.read_csv('all_player_stats.csv')
 data = data.sort_values(by=['SEASON_ID', 'TEAM_ID'])
 
-# filter columns
-# Compute the cosine similarity matrix
-# Select only the columns after the first 6 for the cosine similarity calculation
-cosine_sim = cosine_similarity(data.iloc[1:, 6:])
+# calculate similiarity between players per year
+# Get unique years
+unique_seasons = data['SEASON_ID'].unique()
 
-# Get the top 10 similar players for each player
-top_10_similar = cosine_sim.argsort()[:, -10:]
+# Initialize a dictionary to store cosine similarity matrices for each year
+cosine_sim_by_season = {}
 
-# Now, for a given player, you can recommend other players to trade based on their similarity
-player_id = 2544  # replace with the id of the player you're interested in
-similar_players = top_10_similar[player_id]
+# Loop over each unique year
+for year in unique_seasons:
+    # Subset the data for the current year
+    data_year = data[data['SEASON_ID'] == year]
+    
+    # Compute the cosine similarity matrix for the current year
+    cosine_sim_by_season[year] = cosine_similarity(data_year.iloc[:, 6:])
+
+# cosine_sim_by_season is a dict where the keys are seasons and the values are cosine similarity matrices for that season
+
+
+# Get the cosine similarity matrix for the season of interest
+cosine_sim_season = cosine_sim_by_season['2020-21']
+# Get the top 10 similar players for the player of interest
+player_id = 2544 
+similar_players = cosine_sim_season[player_id].argsort()[-10:]
+
 print(similar_players)
-print(data.iloc[similar_players])
+
 # Create and train the model
 #model = LinearRegression()
 #model.fit(features_train, target_train)
